f06f12bba590b3443dbed98b05f9943b73c9f37e
Marcin Kaczmarek <marcin_kaczmarek@interia.eu>
2019-02-13 17:08:31 +0100

    add modified js files

--- js/Column.js
+++ js/Column.js
@@ -1,12 +1,10 @@
-function Column(name) {
+function Column(id, name) {
     var self = this;
 
-    this.id = randomString();
-    this.name = name;
-    this.element = generateTemplate('column-template', {
-        name: this.name,
-        id: this.id
-    });
+    function Column(id, name) {
+        this.id = id;
+        this.name = name || 'No name given';
+    }
 
     this.element.querySelector('.column').addEventListener('click', function (event) {
         if (event.target.classList.contains('btn-delete')) {
@@ -24,6 +22,56 @@ Column.prototype = {
         this.element.querySelector('ul').appendChild(card.element);
     },
     removeColumn: function () {
-        this.element.parentNode.removeChild(this.element);
+        var self = this;
+        fetch(baseUrl + '/column/' + self.id, {
+                method: 'DELETE',
+                headers: myHeaders
+            })
+            .then(function (resp) {
+                return resp.json();
+            })
+            .then(function (resp) {
+                self.element.parentNode.removeChild(self.element);
+            });
+
+        if (event.target.classList.contains('add-card')) {
+            var cardName = prompt("Enter the name of the card");
+            event.preventDefault();
+
+            fetch(baseUrl + '/card', {
+                    method: 'POST',
+                    body: {
+                        //body query
+                    }
+                })
+                .then(function (res) {
+                    return res.json();
+                })
+                .then(function () {
+                    //create a new client side card
+                });
+
+            self.addCard(new Card(cardName));
+        }
+
+        var data = new FormData();
+        var self = this;
+        data.append('name', cardName);
+        data.append('bootcamp_kanban_column_id', self.id);
+
+        fetch(baseUrl + '/card', {
+                method: 'POST',
+                headers: myHeaders,
+                body: data,
+            })
+            .then(function (res) {
+                return res.json();
+            })
+            .then(function (resp) {
+                var card = new Card(resp.id, cardName);
+                self.addCard(card);
+            });
+
     }
+
 };
